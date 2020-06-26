fa46f8a1393dbaa4df0a41f6e7ad742d9d5a203d
fadlytr <36650539+fadlytr@users.noreply.github.com>
2020-06-26 20:16:07 +0700

    Update kasir
    change index file, and by try php

--- Kasir/Test/BuyTry.php
+++ Kasir/Test/BuyTry.php
@@ -14,35 +14,39 @@ $qlen = strlen($q);
 $idBarang = substr($q, 0, 6);
 $jumlahBarang = substr($q, 7, $qlen);
 
-
+echo $idBarang;
 
 $con = mysqli_connect('localhost','root','','minimarket');
 if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
 
-mysqli_select_db($con,"ajax_demo");
-$sql="SELECT * FROM user WHERE id = '".$q."'";
-$result = mysqli_query($con,$sql);
+// mysqli_select_db($con,"minimarket");
+$getStok="SELECT stok_barang FROM barang WHERE id_barang = '".$idBarang."'";
+$stokNow = mysqli_query($con,$getStok);
+$stokNow = $stokNow - $jumlahBarang;
+$updateStok = "UPDATE barang SET stok_barang = $stokNow WHERE id_barang = '".$idBarang."'";
+mysqli_query($con,$updateStok);
 
-echo "<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-<th>Age</th>
-<th>Hometown</th>
-<th>Job</th>
-</tr>";
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  echo "<td>" . $row['FirstName'] . "</td>";
-  echo "<td>" . $row['LastName'] . "</td>";
-  echo "<td>" . $row['Age'] . "</td>";
-  echo "<td>" . $row['Hometown'] . "</td>";
-  echo "<td>" . $row['Job'] . "</td>";
-  echo "</tr>";
-}
-echo "</table>";
+echo $stokNow;
+// echo "<table>
+// <tr>
+// <th>Firstname</th>
+// <th>Lastname</th>
+// <th>Age</th>
+// <th>Hometown</th>
+// <th>Job</th>
+// </tr>";
+// while($row = mysqli_fetch_array($result)) {
+//   echo "<tr>";
+//   echo "<td>" . $row['FirstName'] . "</td>";
+//   echo "<td>" . $row['LastName'] . "</td>";
+//   echo "<td>" . $row['Age'] . "</td>";
+//   echo "<td>" . $row['Hometown'] . "</td>";
+//   echo "<td>" . $row['Job'] . "</td>";
+//   echo "</tr>";
+// }
+// echo "</table>";
 mysqli_close($con);
 ?>
 </body>
