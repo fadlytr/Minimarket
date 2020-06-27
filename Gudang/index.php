1fd308cb6abb1c737d19ea06b56552131a81755d
regawarp <regawa04@gmail.com>
2020-06-27 09:45:36 +0700

    Create index.php

---
+++ Gudang/index.php
@@ -0,0 +1,29 @@
+<html>
+<head></head>
+<body>
+	<h1>Helo</h1>
+</body>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDGI9VjRtkXmEHXSFIMyVvyJlhmtGCLGqI",
+    authDomain: "minimarket-c18b4.firebaseapp.com",
+    databaseURL: "https://minimarket-c18b4.firebaseio.com",
+    projectId: "minimarket-c18b4",
+    storageBucket: "minimarket-c18b4.appspot.com",
+    messagingSenderId: "1009663674856",
+    appId: "1:1009663674856:web:ebd523eaf2fa040d61bc5d",
+    measurementId: "G-CDB0TYJRJQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+</html>
