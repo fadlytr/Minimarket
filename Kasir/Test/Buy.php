a393f1092bee6b1e73864fc64991e51f4bdffee8
fadlytr <36650539+fadlytr@users.noreply.github.com>
2020-07-17 12:27:54 +0700

    cart sudah tampil
    cart sudah tampil  namun pembayaran masih gagal

--- Kasir/Test/Buy.php
+++ Kasir/Test/Buy.php
@@ -1,25 +1,30 @@
 <?php
 include 'connection.php';
-if (isset($_POST['idBarangBuy']) && isset($_POST['jumlahBuy'])) {
-  $idBarang = $_POST['idBarangBuy'];
-  $jumlahBarang = $_POST['jumlahBuy'];
+if (isset($_POST['Buy'])) {
+  if (isset($_POST['idBarangBuy']) && isset($_POST['jumlahBuy'])) {
+    $idBarang = $_POST['idBarangBuy'];
+    $jumlahBarang = $_POST['jumlahBuy'];
+    $datenow = date("Y-m-d H:i:s");
 
-  if ($idBarang != NULL && $jumlahBarang != NULL) {
-    // echo $idBarang;
-    // echo $jumlahBarang;
+    if ($idBarang != NULL && $jumlahBarang != NULL) {
+      // echo $idBarang;
+      // echo $jumlahBarang;
 
-    $updateStok = "UPDATE barang SET stok_barang = stok_barang - $jumlahBarang WHERE id_barang = '$idBarang'";
+      $updateStok = "UPDATE barang SET stok_barang = stok_barang - $jumlahBarang WHERE id_barang = '$idBarang'";
+      $insetTransaksi = "INSERT into transaksi(jenis_trx,tgl_trx,status) VALUES ('1', '$datenow', 'bayar')";
+      mysqli_query($conn, $insetTransaksi);
 
-    if (mysqli_query($conn, $updateStok)) {
-      echo "Pembelian berhasil!";
+      if (mysqli_query($conn, $updateStok)) {
+        echo "Pembelian berhasil!";
+      } else {
+        echo "Pembelian gagal!";
+      }
     } else {
-      echo "Pembelian gagal!";
+      echo "Pembelian gagal2!";
     }
   } else {
-    echo "Pembelian gagal2!";
+    echo "Pembelian gagal3!";
   }
-} else {
-  echo "Pembelian gagal3!";
 }
 ?>
 <form method="post" action="index.php">
