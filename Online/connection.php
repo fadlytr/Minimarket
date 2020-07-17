d26fdafa63f0f16ec107108493d90d50005683b1
alifiasyalsa <alifiasyalsabila96@gmail.com>
2020-07-17 16:53:08 +0700

    Update connection.php

--- Online/connection.php
+++ Online/connection.php
@@ -1,28 +1,28 @@
 <?php
-  // //buat koneksi dengan database mysql
-  // $dbhost = "wisata-lembang.com";
-  // $dbuser = "u8128384";
-  // $dbpass = "ca6rHxbjt#";
-  // $dbname = "u8128384_minimarket";
-  // $link = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-  
-  // //periksa koneksi, tampilkan pesan kesalahan jika gagal
-  // if(!$link){
-  //  die ("Koneksi dengan database gagal: ".mysqli_connect_errno().
-  //        " - ".mysqli_connect_error());
-  //  }
-
-
-  // buat koneksi dengan database mysql
-  $dbhost = "localhost";
-  $dbuser = "root";
-  $dbpass = "";
-  $dbname = "minimarket";
+  //buat koneksi dengan database mysql
+  $dbhost = "wisata-lembang.com";
+  $dbuser = "u8128384";
+  $dbpass = "ca6rHxbjt#";
+  $dbname = "u8128384_minimarket";
   $link = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
   
   //periksa koneksi, tampilkan pesan kesalahan jika gagal
   if(!$link){
-    die ("Koneksi dengan database gagal: ".mysqli_connect_errno().
+   die ("Koneksi dengan database gagal: ".mysqli_connect_errno().
          " - ".mysqli_connect_error());
-  } 
+   }
+
+
+  // buat koneksi dengan database mysql
+  // $dbhost = "localhost";
+  // $dbuser = "root";
+  // $dbpass = "";
+  // $dbname = "minimarket";
+  // $link = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+  
+  // //periksa koneksi, tampilkan pesan kesalahan jika gagal
+  // if(!$link){
+  //   die ("Koneksi dengan database gagal: ".mysqli_connect_errno().
+  //        " - ".mysqli_connect_error());
+  // } 
 ?>  
